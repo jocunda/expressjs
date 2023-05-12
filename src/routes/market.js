1e7ba7af9b4a51ab1dd342039d682078e40f08fa
jocunda <silviawanshan88@gmail.com>
2023-05-12 21:46:43 +0800

    queryParameter

--- src/routes/market.js
+++ src/routes/market.js
@@ -4,18 +4,39 @@ const router = Router();
 
 const supermarkets = [
   {
+    id: 1,
     store: "Walmart",
+    miles: 0.6,
   },
   {
+    id: 2,
     store: "Fairprice",
+    miles: 2.5,
   },
   {
+    id: 3,
     store: "Costco",
+    miles: 2.8,
+  },
+  {
+    id: 4,
+    store: "Giant",
+    miles: 3.5,
+  },
+  {
+    id: 5,
+    store: "Kaisar",
+    miles: 1.8,
   },
 ];
 
 router.get("", (request, response) => {
-  response.send(supermarkets);
+  const { miles } = request.query;
+  const parsedMiles = parseInt(miles);
+  if (!isNaN(parsedMiles)) {
+    const filterStores = supermarkets.filter((s) => s.miles <= parsedMiles);
+    response.send(filterStores);
+  } else response.send(supermarkets);
 });
 
 module.exports = router;
